cc5332a6baf2d8d7ae2df3ceaf75a62b8e6fa082
Renjun Xu <renjun.xu@amlogic.com>
2017-07-17 16:09:34 +0800

    modified for scons missing err
    Change-Id: I4fd644e2532d006d9586e230ac90073198524d88

--- upnp-app/upnp-app.mk
+++ upnp-app/upnp-app.mk
@@ -9,13 +9,13 @@ UPNP_APP_SITE=$(TOPDIR)/../vendor/amlogic/external/platinum/upnp-app/src
 UPNP_APP_SITE_METHOD=local
 UPNP_APP_BUILD_DIR = $(BUILD_DIR)
 UPNP_APP_INSTALL_STAGING = YES
-UPNP_APP_DEPENDENCIES = gstreamer1 host-pkgconf libplayer
+UPNP_APP_DEPENDENCIES = gstreamer1 host-pkgconf libplayer host-scons
 CFLAGS = -W1,-fPIC
 CURRENT_DIR = $(UPNP_APP_BUILD_DIR)/$(appName)-$(UPNP_APP_VERSION)/
 
 OUT_BIN = $(TARGET_DIR)/sbin
 define UPNP_APP_BUILD_CMDS
-	cd $(UPNP_APP_SITE);scons -c target=arm-unknown-linux outdir=$(OUT_BIN);scons target=arm-unknown-linux dir=$(STAGING_DIR) outdir=$(OUT_BIN) \
+	cd $(CURRENT_DIR);${SCONS} -c target=arm-unknown-linux outdir=$(OUT_BIN);${SCONS} target=arm-unknown-linux dir=$(STAGING_DIR) outdir=$(OUT_BIN) \
 	build_config=Debug gcctool=$(TARGET_CC)
 endef
 
@@ -24,7 +24,7 @@ define DEMO_APP_INSTALL_TARGET_CMDS
 endef
 
 define UPNP_APP_CLEAN_CMDS
-	cd $(CURRENT_DIR);scons -c target=arm-unknown-linux outdir=$(OUT_BIN)
+	cd $(CURRENT_DIR);${SCONS} -c target=arm-unknown-linux outdir=$(OUT_BIN)
 endef
 
 $(eval $(generic-package))
